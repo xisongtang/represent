4e1bdc80e7ea183830f062b4904bed9b706b8d20
xisong.tang <xisong.tang@qq.com>
2015-06-27 19:40:24 +0800

    pointer locate correct

--- app/js/app.js
+++ app/js/app.js
@@ -1,8 +1,8 @@
 'use strict';
 
-var panelX = ((+$(".section-wrapper").css("left").slice(0, -2)) + (+$(".section-wrapper").css("padding-left").slice(0, -2)) + 
+var panelX = ((+$(".rel-wrapper").css("margin-left").slice(0, -2)) + (+$(".rel-wrapper").css("padding-left").slice(0, -2)) + 
 						(+$("section").css("margin-left").slice(0, -2))),
-						panelY = ((+$(".section-wrapper").css("top").slice(0, -2)) + (+$(".section-wrapper").css("padding-top").slice(0, -2)) + 
+		panelY = ((+$(".rel-wrapper").prop("offsetTop")) + (+$(".rel-wrapper").css("padding-top").slice(0, -2)) + 
 						(+$("section").css("margin-top").slice(0, -2)));
             
 // Declare app level module which depends on views, and components
